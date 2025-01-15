dc2c908974cbfcad6b8f43737b03a96a0e3431a2
Marek Sterzik <marek.sterzik@gmail.com>
2025-01-15 22:48:32 +0100

    implement full table generator

--- src/TableGenerator/TableGenerator.php
+++ src/TableGenerator/TableGenerator.php
@@ -3,14 +3,27 @@
 namespace App\TableGenerator;
 
 use Symfony\Component\Yaml\Yaml;
+use Exception;
 
 class TableGenerator
 {
-    private array $data;
+    private array $data = [];
 
     public function __construct(string $dataFile)
     {
-        $this->data = Yaml::parseFile($dataFile);
+        $data = @file_get_contents($dataFile);
+        if (is_string($data)) {
+            $data = base64_decode($data);
+            if (is_string($data)) {
+                try {
+                    $data = Yaml::parse($data);
+                    if (is_array($data)) {
+                        $this->data = $data;
+                    }
+                } catch (Exception $e) {
+                }
+            }
+        }
     }
 
     public function getTableData(string $tableIdentifier): ?array
